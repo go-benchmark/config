4792a22dbd6cd6a659cf5b1e57f985ceffa17459
nguyencatpham <dtpham258@gmail.com>
2020-08-16 13:20:35 +0700

    fix config path

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func init() {
 	viper.SetConfigName("default") // name of config file (without extension)
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")                                                      // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("$HOME/go/pkg/mod/github.com/go-benchmark/config@v0.0.1") // path to look for the config file in
+	viper.AddConfigPath("$HOME/go/pkg/mod/github.com/go-benchmark/config@v0.0.2") // path to look for the config file in
 	err := viper.ReadInConfig()                                                   // Find and read the config file
 	if err != nil {                                                               // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %s ", err))
